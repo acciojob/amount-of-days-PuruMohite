60e8390e2cdf67dbc155b31fceb5d05afe282ab3
PuruMohite <107110840+PuruMohite@users.noreply.github.com>
2026-04-10 19:20:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	if(year%4){
+		if(year%100){
+			if(year%400){
+				return 366;
+			}
+			else{
+				return 365;
+			}
+		}else{
+			return 366;
+		}
+	}else{
+		return 365;
+	}
+}
+
+
